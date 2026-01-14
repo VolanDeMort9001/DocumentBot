af8dff0974f3ce1a9055ff9280ceeefbba8039bd
VolanDeMort9001 <alex.nazarov.200306@gmail.com>
2026-01-15 00:24:46 +0300

    Added another summary type

--- models/models.py
+++ models/models.py
@@ -8,8 +8,10 @@ def create_document(text: Iterable[str]) -> None:
     doc = Document()
     print(text)
     doc.add_paragraph("Коллеги, добрый день!\n\nБлагодарим за вовремя присланный мониторинговый отчет.\nПо результатам его проверки просим учесть следующие рекомендации и доработать отчет в срок до:\n")
+    number = 1
     for par in text:
-        doc.add_paragraph(par)
+        doc.add_paragraph(f"{number}. {par}")
         doc.add_paragraph("")
+        number += 1
     doc.add_paragraph("Ждем доработанный отчет. Если возникнут вопросы, я на связи. Мои контакты отображены на площадке. Буду рада помочь.")
     doc.save("ABS_отчет.docx")
